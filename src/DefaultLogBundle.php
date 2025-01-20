5789143cd3f80794702ff4400bcf1139c0240ff6
Jochen Schultz <jschultz@php.net>
2025-01-20 03:35:25 +0100

    Update namespaces and docblocks for v1.0.0

--- src/DefaultLogBundle.php
+++ src/DefaultLogBundle.php
@@ -2,6 +2,7 @@
 
 namespace IntelligentIntern\DefaultLogBundle;
 
+use App\DependencyInjection\Compiler\LogStrategyCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -11,5 +12,12 @@ class DefaultLogBundle extends AbstractBundle
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import(__DIR__ . '/../config/services.yaml');
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new LogStrategyCompilerPass());
     }
 }
